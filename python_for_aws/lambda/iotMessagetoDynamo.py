1408da2e0349b9641517a38823c5564f19a2cda0
AlirezaPourchali <arp.joker82@gmail.com>
2024-02-14 19:59:04 +0330

    iot scenerio for skills

---
+++ python_for_aws/lambda/iotMessagetoDynamo.py
@@ -0,0 +1,19 @@
+import json
+import time
+import boto3
+import math
+
+def lambda_handler(event, context):
+    message = event['msg']
+    client = boto3.client('comprehend' , region_name="us-east-1")
+    sen = client.detect_sentiment(Text=message , LanguageCode='en')['Sentiment']
+    
+    dynamodb = boto3.client('dynamodb' , region_name='us-east-1')
+    t = math.ceil(time.time())
+    table='mytable'
+        
+    dynamodb.put_item(TableName=table , Item={'id': {'N': f'{t}'}, 'sentiment': {'S': f'{sen}'}  , 'message' :  {'S' : f"{message}" } })
+    
+    
+    
+    return 200
