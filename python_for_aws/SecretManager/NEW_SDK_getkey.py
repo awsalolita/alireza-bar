1b793218529abd8d2738bc8c80e221fcc6a0438d
AlirezaPourchali <arp.joker82@gmail.com>
2024-04-26 11:58:02 +0330

    feat: jsonify secret

--- python_for_aws/SecretManager/NEW_SDK_getkey.py
+++ python_for_aws/SecretManager/NEW_SDK_getkey.py
@@ -2,11 +2,11 @@
 
 import boto3
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
-
+import json
 
 client = boto3.client('secretsmanager' , region_name = 'us-east-1')
 cache_config = SecretCacheConfig()
 cache = SecretCache( config = cache_config, client = client)
 secret = cache.get_secret_string('mysecret')
-
+a = json.loads(secret)
 
