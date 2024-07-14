d7c1a9bcc1b59ace75fabbdaa033a333728d2e7f
AlirezaPourchali <arp.joker82@gmail.com>
2024-07-14 14:29:40 +0330

    Rearrange

--- Athena/apache.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS `apache_servers`.`logs` (`host` string, `method` string, `code` string)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
-WITH SERDEPROPERTIES ('field.delim' = ',')
-STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://sooper-dooper-apache-logs-002/logs/'
-TBLPROPERTIES ('classification' = 'csv');
--- CDK/ec2.sh
+++
@@ -1,5 +0,0 @@
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-nvm install 20
-npm install -g aws-cdk
